aa6f7114715fa5273614670ec927bb6437606f50
RA302 <RA302@10.0.35.208>
2013-11-20 17:29:19 -0500

    se agregaron las clases para el manejo de archivos y de las canciones

---
+++ ConcertSocket/src/logic/Song.java
@@ -0,0 +1,62 @@
+/**
+ * esta clase se encarga de .
+ */
+package logic;
+
+import java.util.ArrayList;
+
+import persistence.FileManager;
+
+/**
+ * @author Juan Nicolas Martinez Fagua
+ *
+ */
+public class Song {
+
+	private FileManager fileManager;
+	private ArrayList<String> letter;
+	private String name;
+	/**
+	 * este es el metodo constructor de la clase Song.java
+	 */
+	public Song(String name) {
+		this.name = name;
+		letter = new ArrayList<String>();
+		fileManager = new FileManager(name);
+	}
+	
+	public void updateSong(){
+		fileManager.readFile();
+		letter = fileManager.getLetter();
+	}
+
+	/**
+	 * @return the letter
+	 */
+	public ArrayList<String> getLetter() {
+		return letter;
+	}
+
+	/**
+	 * @param letter the letter to set
+	 */
+	public void setLetter(ArrayList<String> letter) {
+		this.letter = letter;
+	}
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+}
---
+++ ConcertSocket/src/persistence/FileManager.java
@@ -0,0 +1,85 @@
+/**
+ * esta clase se encarga de .
+ */
+package persistence;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.ArrayList;
+
+/**
+ * @author Juan Nicolas Martinez Fagua
+ *
+ */
+public class FileManager {
+
+	private BufferedReader bufferedReader;
+	private File file;
+	private FileReader fileReader;
+	private ArrayList<String> letter;
+	private String name;
+	
+	/**
+	 * este es el metodo constructor de la clase File.java
+	 */
+	public FileManager(String name) {
+		file = new File("");
+		this.name = file.getAbsolutePath() + name + ".music";
+		letter = new ArrayList<String>();
+	}
+	
+	public void openFile(){
+		try {
+			fileReader = new FileReader(name);
+			bufferedReader = new BufferedReader(fileReader);
+		} catch (FileNotFoundException e) {
+			System.out.println("");
+		}
+		
+	}
+	
+	public void readFile(){
+		String line = "";
+		openFile();
+		try {
+			while ((line = bufferedReader.readLine()) != null ) {
+				letter.add(line);
+			}
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+	}
+
+	/**
+	 * @return the letter
+	 */
+	public ArrayList<String> getLetter() {
+		return letter;
+	}
+
+	/**
+	 * @param letter the letter to set
+	 */
+	public void setLetter(ArrayList<String> letter) {
+		this.letter = letter;
+	}
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	
+}
