19875769cae1b27aacdd666370492b89e0e60454
Juan Nicolas Martinez <RA302@ACWIN7RA302-18>
2013-11-21 12:00:06 -0500

    creacion ventana de inicio del servidor

--- ConcertSocket/src/test/MainServer.java
+++ ConcertSocket/src/test/MainServer.java
@@ -3,16 +3,53 @@
  */
 package test;
 
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
 import logic.Server;
 
 /**
  * @author Juan Nicolas Martinez Fagua
  *
  */
-public class MainServer {
+public class MainServer extends JFrame implements ActionListener{
 
+	private JButton btnCreate;
+	
+	/**
+	 * este es el metodo constructor de la clase MainServer.java
+	 */
+	public MainServer() {
+		super("Generadpr de Servidores");
+		setSize(200, 200);
+		setLayout(null);
+		
+		btnCreate = new JButton("Crear Servidor");
+		btnCreate.setBounds(30, 90, 150, 30);
+		btnCreate.addActionListener(this);
+		
+		
+		setVisible(true);
+	}
+	
 	public static void main(String[] args) {
-		Server server = new Server();
-		server.initServer();
+//		Server server = new Server();
+//		server.initServer();
+		
+	}
+
+	/* (non-Javadoc)
+	 * @see java.awt.event.ActionListener#actionPerformed(java.awt.event.ActionEvent)
+	 */
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		if(arg0.getActionCommand().equals("Crear Servidor")){
+			
+		}
+		
 	}
 }
